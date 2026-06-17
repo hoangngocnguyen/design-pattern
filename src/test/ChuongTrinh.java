9603a0b4d60f0d45aa0792ebcd2c67dbc9d8d918
hoangngocnguyen <142553243+hoangngocnguyen@users.noreply.github.com>
2026-06-17 16:04:23 +0700

    first commit

---
+++ src/test/ChuongTrinh.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.sql.SQLException;
+
+public class ChuongTrinh {
+	public static void main(String[] args) throws SQLException {
+		System.out.println("Hello world");
+		KetNoi a = KetNoi.getInstance();
+	}
+}
---
+++ src/test/KetNoi.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KetNoi {
+	private static KetNoi instance;
+	private Connection connection;
+
+	private static final String URL = "jdbc:mysql://localhost:3306/hoangshopdb";
+	private static final String USER = "root";
+	private static final String PASSWORD = "123456";
+
+	private KetNoi() throws SQLException {
+	    try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+	        System.out.println("Kết nối với CSDL thành công!");
+	    } catch (ClassNotFoundException e) {
+	        System.out.println("Lỗi: Không tìm thấy Driver MySQL!");
+	        throw new SQLException("MySQL JDBC Driver not found.", e);
+	    } catch (SQLException e) {
+	        // Đây là nơi chương trình sẽ nhảy vào khi Docker bị tắt
+	        System.out.println("Kết nối với CSDL thất bại!.");
+	        throw e; // Ném lỗi lên để nơi gọi hàm biết mà xử lý
+	    }
+	}
+
+	public static KetNoi getInstance() throws SQLException {
+		if (instance == null || instance.getConnection().isClosed()) {
+			instance = new KetNoi();
+		}
+		return instance;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+}
